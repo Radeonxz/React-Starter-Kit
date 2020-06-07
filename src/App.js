21de0175e30438812f130bfe6fe148b0df7723a5
Radeonxz <xuanxxzhao@gmail.com>
2020-06-07 18:37:55 -0400

    feat: refator structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React from "react";
+import AppContainer from "./containers/AppContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <AppContainer />;
+};
 
 export default App;
---
+++ src/containers/WelcomeContainer/WelcomeContainer.js
@@ -0,0 +1,13 @@
+/**
+ * WelcomeContainer
+ */
+import React from "react";
+
+// import components
+import Welcome from "../../components/Welcome";
+
+const WelcomeContainer = () => {
+  return <Welcome />;
+};
+
+export default WelcomeContainer;
