6ff78bc4710c62e794765fdd2c49351498e93bd2
Radeonxz <xuanxxzhao@gmail.com>
2020-06-08 21:59:01 -0400

    feat: add config

---
+++ src/config/config.js
@@ -0,0 +1,30 @@
+import merge from "merge";
+import baseConfig from "./app.config.dev.json";
+import devConfig from "./app.config.dev.json";
+import prodConfig from "./app.config.prod.json";
+
+const envConfigs = {
+  dev: devConfig,
+  prod: prodConfig
+};
+
+/**
+ * Puts together the configuration, as combination of base and environment-specific config
+ *
+ * @param {string} env Current app environment
+ * @returns {object} Configuration object
+ */
+export const getConfig = (env) => {
+  let allConfig = baseConfig;
+  let envConfig;
+  if (env) {
+    envConfig = envConfigs[env];
+  } else {
+    envConfig = envConfigs.dev;
+  }
+
+  if (envConfig) {
+    allConfig = merge.recursive(true, allConfig, envConfig);
+  }
+  return allConfig;
+};
