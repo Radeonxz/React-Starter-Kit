afdf8300e50028986e1eb95e8b651ca27fce93a1
Radeonxz <xuanxxzhao@gmail.com>
2020-06-12 23:38:25 -0400

    chore: update navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,9 +2,25 @@
  * Navigation
  */
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./styled/Navigation.css";
 
 const Navigation = ({ changeLocale }) => {
-  return <p onClick={() => changeLocale("fr-ca")}>Navigation</p>;
+  return (
+    <header className="main-navigation">
+      <div className="main-navigation_logo">
+        <h3>React Start Kit</h3>
+      </div>
+      <div className="main-navigation_items">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li onClick={() => changeLocale("fr-ca")}>Change Locale</li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Navigation;
