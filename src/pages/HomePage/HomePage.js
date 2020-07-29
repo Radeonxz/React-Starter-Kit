d6a39da0760a5adf6f4b538b7743c0c6d3dcca20
Radeonxz <xuanxxzhao@gmail.com>
2020-07-28 22:43:39 -0400

    chore: rollback to use navibar

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -3,17 +3,14 @@
  */
 import React from "react";
 
-import Dropdown from "../../components/Dropdown";
-
 // import containers
-// import NavigationContainer from "../../containers/NavigationContainer";
+import NavigationContainer from "../../containers/NavigationContainer";
 import WelcomeContainer from "../../containers/WelcomeContainer";
 
 const HomePage = () => {
   return (
     <>
-      {/* <NavigationContainer /> */}
-      <Dropdown />
+      <NavigationContainer />
       <WelcomeContainer />
     </>
   );
