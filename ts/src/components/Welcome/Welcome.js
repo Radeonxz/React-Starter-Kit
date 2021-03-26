ad3a39f4796f6af412f369452b0f445e9d9097e4
RadeonXz <xuanxxzhao@gmail.com>
2021-03-25 22:58:54 -0400

    chore: add welcome

---
+++ ts/src/components/Welcome/Welcome.js
@@ -0,0 +1,26 @@
+/**
+ * Welcome
+ */
+import "./styled/Welcome.css";
+import logo from "./logo.svg";
+
+const Welcome = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to use React starter kit.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Welcome;
