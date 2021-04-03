ed888e2c4e545d77ea7a2d895052fd8e91d324e0
RadeonXz <xuanxxzhao@gmail.com>
2021-04-03 11:13:24 -0400

    chore: add appContainer

---
+++ ts/src/containers/AppContainer/AppContainer.tsx
@@ -0,0 +1,13 @@
+/**
+ * AppContainer
+ */
+import { BrowserRouter } from "react-router-dom";
+
+const AppContainer = () => {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  );
+};
+
+export default AppContainer;
