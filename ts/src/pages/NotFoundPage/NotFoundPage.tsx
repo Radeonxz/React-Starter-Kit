44963bf85fd6cc2e334c536867a38b2f6bdc525c
RadeonXz <xuanxxzhao@gmail.com>
2021-04-06 23:46:40 -0400

    chore: add not found page

---
+++ ts/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,16 @@
+/**
+ * NotFoundPage
+ */
+
+// import containers
+import NotFoundContainer from "../../containers/NotFoundContainer";
+
+const NotFoundPage = () => {
+	return (
+		<>
+			<NotFoundContainer />
+		</>
+	);
+};
+
+export default NotFoundPage;
