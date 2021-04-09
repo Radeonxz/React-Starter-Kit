dcd75241e86e4e5ebb576bdcbf2df46bfe64e0a7
RadeonXz <xuanxxzhao@gmail.com>
2021-04-08 23:49:05 -0400

    chore: config store

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,12 +1,21 @@
+import { Provider } from "react-redux";
+
+// import configureStore, sagas, app container wrapper
+import configureStore from "./modules/store/configureStore";
+import moduleSagas from "./modules/store/moduleSagas";
+
 import WelcomeContainer from "./containers/WelcomeContainer";
 import AppContainer from "./containers/AppContainer";
 
 const App = () => {
+	const store = configureStore();
+	store.runSaga(moduleSagas);
+
 	return (
-		<>
+		<Provider store={store}>
 			<AppContainer />
 			<WelcomeContainer />
-		</>
+		</Provider>
 	);
 };
 
---
+++ ts/src/modules/store/moduleSagas.js
@@ -0,0 +1,12 @@
+/**
+ * Module Sagas List
+ */
+import { /* fork, */ all } from "redux-saga/effects";
+
+// import your sagas
+
+export default function* root() {
+  yield all([
+    // inject imported sagas
+  ]);
+}
