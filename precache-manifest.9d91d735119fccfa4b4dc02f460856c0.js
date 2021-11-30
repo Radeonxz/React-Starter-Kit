29313118b1bf197f86341d48977555f2e4688634
RadeonXz <xuanxxzhao@gmail.com>
2021-11-29 19:37:17 -0500

    Updates

---
+++ precache-manifest.9d91d735119fccfa4b4dc02f460856c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3dc6e27078a64ffa979b76cbecbd20",
+    "url": "/React-Starter-Kit/index.html"
+  },
+  {
+    "revision": "3888afc50f18687c2097",
+    "url": "/React-Starter-Kit/static/css/main.ae083b37.chunk.css"
+  },
+  {
+    "revision": "51cd2aed70a807660bc1",
+    "url": "/React-Starter-Kit/static/js/2.db76b503.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/React-Starter-Kit/static/js/2.db76b503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3888afc50f18687c2097",
+    "url": "/React-Starter-Kit/static/js/main.f3f06bd1.chunk.js"
+  },
+  {
+    "revision": "2c6a87900c2765887616",
+    "url": "/React-Starter-Kit/static/js/runtime-main.9681620a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/React-Starter-Kit/static/media/logo.ee7cd8ed.svg"
+  }
+]);
